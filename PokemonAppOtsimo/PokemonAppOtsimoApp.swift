f7d4b3ca31b3b19a44716223520a3e99ef1851cb
BurakOztopuz <boztopuz@gmail.com>
2022-08-16 11:52:39 +0300

    Initial Commit

---
+++ PokemonAppOtsimo/PokemonAppOtsimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppOtsimoApp.swift
+//  PokemonAppOtsimo
+//
+//  Created by Burak Öztopuz on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppOtsimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
